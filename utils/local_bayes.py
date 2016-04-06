7d833549e5b943544b31c70eae607b9a414531ce
uchihashi_k <uchihashi.kenshi@gmail.com>
2016-04-07 05:25:52 +0900

    local bayesの部分関数

--- utils/local_bayes.py
+++ utils/local_bayes.py
@@ -5,6 +5,18 @@ import six
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 import pulp
+import math
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import cuda
+from chainer import optimizers
+from chainer import serializers
+from chainer import Link, Chain, ChainList
 
 class LocalBayes():
 
@@ -13,13 +25,13 @@ class LocalBayes():
         self.data_dim = data_dim
 
 
-    def get_nearest_n(self, train, test, n=20, max_dist=50):
+    def get_nearest_n(self, train, test, max_dist=50):
         """
         :param train: Training dataset. Must be pandas object.
         :param test: Some point of test data. Must be numpy array object.
         :param n: Number of training data in the neighborhood of one test data point.
         :param max_dist: Max distance between train and test.
-        :return: The nearest points of training dataset (with any metrics).
+        :return: The nearest points of training dataset (with DTW metrics).
         """
         nn_dist_ls, nn_ts_ls = numpy.array([]), []
         te_ele = numpy.array(test).reshape(-1, 1)
@@ -31,12 +43,14 @@ class LocalBayes():
             tr_ele = numpy.array(tr_ele).reshape(-1, 1)
             dist, path = fastdtw(te_ele, tr_ele, dist=euclidean)
 
-            if len(nn_dist_ls) < n:
+            if len(nn_dist_ls) < self.nn_num:
                 nn_dist_ls = numpy.append(nn_dist_ls, dist)
                 nn_ts_ls.append(tr_ele_ls)
             elif numpy.max(nn_dist_ls) > dist:
+
                 if numpy.max(nn_dist_ls) < max_dist:
                     break
+
                 max_ind = numpy.argmax(nn_dist_ls)
                 nn_dist_ls[max_ind] = dist
                 nn_ts_ls[max_ind] = tr_ele_ls
@@ -46,8 +60,29 @@ class LocalBayes():
         return nn_dist_ls, nn_ts_ls
 
 
-    def cal_prediction_nearest_n(self):
+    def cal_prediction_nearest_n(self, nn_ts_ls, learner_name_ls, model_dict):
+        """
+        todo: local_bayes_predictと被ってる・・・
+        :param nn_ts_ls: Nearest n data. Must be numpy.array object.
+        :param learner_name_ls: List of learner name. For example, cnn, gbdt, logi, and more.
+        :param model_dict: Dict of trained models.
+        :return:
+        """
 
+        nn_pred_dict = {}
+        for learner in learner_name_ls:
+            if learner == 'cnn':
+                x = nn_ts_ls.reshape((self.nn_num, 1, 1, self.data_dim)).astype(numpy.float32)
+                pred = F.softmax(model_dict[learner].predictor(chainer.Variable(x))).data[0][1]
+            elif learner == 'gbdt':
+                pred = numpy.squeeze(model_dict[learner].predict_proba(test_data)).tolist()[1]
+            else:
+                print "not enough parameters"
+                break
+
+            nn_pred_dict.update({'%s' % learner:pred})
+
+        return 0
 
 
     def local_bayes_estimation(self, pred_proba_ls, learner_name_ls):
@@ -55,21 +90,44 @@ class LocalBayes():
         problem = pulp.LpProblem('localbayes', pulp.LpMaximize)
         var = pulp.LpVariable.dicts('w', (learner_name_ls), 0, 1, 'Continuous')
 
-        for i in six.moves.range(20):
-            cnn = pred_[i] + 0.0000001
-            gbdt = pred_proba[i][1]
-            model_sum = var[1] * math.log(cnn) + var[2] * math.log(gbdt)
+        ## objective
+        for j in six.moves.range(20):
+            model_sum = 0
+
+            for i, learner in enumerate(learner_name_ls):
+                model_sum += var[learner] * math.log(pred_proba_ls[i][j] + 0.000000001)
+
             problem += model_sum
 
-        for learner in learner_name:
+        ##  constraint
+        problem_ = 0
+        for learner in learner_name_ls:
             problem += var[learner] >= 0
+            problem_ += var[learner]
 
-        for learner in learner_name:
-            problem += var[learner]
-        problem == 1
+        problem += problem_ == 1
+        problem.solve()
 
-        return
+        w_dict = {}
+        for learner in learner_name_ls:
+            w_dict.update({'%s' % learner:var[learner].value()})
+
+        return w_dict
 
 
-    def local_bayes_predict(self):
-        pass
+    def local_bayes_predict(self, test_data, learner_name_ls, w_dict, model_dict):
+
+        lb_pred = 0
+
+        for learner in learner_name_ls:
+
+            if learner == 'cnn':
+                x = test_data.reshape((1, 1, 1, self.data_dim)).astype(numpy.float32)
+                lb_pred += w_dict[learner] * F.softmax(model_dict[learner].predictor(chainer.Variable(x))).data[0][1]
+            elif learner == 'gbdt':
+                lb_pred += numpy.squeeze(model_dict[learner].predict_proba(test_data)).tolist()[1]
+            else:
+                print "not enough parameters"
+                break
+
+        return lb_pred
